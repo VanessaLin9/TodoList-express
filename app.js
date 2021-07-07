20b9413527a63fab64164211db22fd0ecc8217f1
VanessaLin9 <vanessa7591@gmail.com>
2021-07-08 05:21:58 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
 
-//Template engine
+const mongoose = require('mongoose') //載入 mongoose
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true}) //設定連線到MongoDB
 
-// setting body-parser
+//取得資料庫連線狀態
+const db = mongoose.connection
+//連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+//連線成功
+db.once('open', () => {
+  console.log('mongodb conneted!')
+})
 
 app.get('/', (req,res) => {
   res.send('hello! world!')
