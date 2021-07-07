b4d1fe11442edf8b1d4e4f37686502acdcec0580
VanessaLin9 <vanessa7591@gmail.com>
2021-07-08 05:44:17 +0800

    feat: add todo seeder

---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Todo = require('../todo') //載入todo model
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('monodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (i=0; i<10; i++){
+    Todo.create({name: 'name-' + i })
+  }
+  console.log('done!')
+})
