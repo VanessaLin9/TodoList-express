ba80d86487b2ec7d23194f985a914f5c026a2832
VanessaLin9 <vanessa7591@gmail.com>
2021-07-09 04:42:12 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express()
 const exphbs = require('express-handlebars') //引用express-handlebars 並且命名為"exphbs"
 const port = 3000
 
-app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'})) //建立一個名為'hbs'的樣板引擎, 並且傳入exphbs 與相關參數
+const Todo = require('./models/todo') //載入todo model
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) //建立一個名為'hbs'的樣板引擎, 並且傳入exphbs 與相關參數
 app.set('view engine', 'hbs') //啟用樣板引擎 hbs
 
 const mongoose = require('mongoose') //載入 mongoose
 const { mainModule } = require('process')
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true}) //設定連線到MongoDB
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) //設定連線到MongoDB
 
 //取得資料庫連線狀態
 const db = mongoose.connection
@@ -21,10 +23,12 @@ db.once('open', () => {
   console.log('mongodb conneted!')
 })
 
-app.get('/', (req,res) => {
-  res.render('index')
+app.get('/', (req, res) => {
+  Todo.find() //取出Todo model 裡的所有資料
+    .lean() //把Mongoose 的 model 物件轉換成乾淨的 JS 資料陣列
+    .then( todos => res.render('index', {todos})) //將資料傳給 index 樣板, 這邊{todos}是{todos: todo}的縮寫
+    .catch(error => console.error(error)) //錯誤處理
 })
-
 
 app.listen(port, () => {
   console.log(`Express is listen on localhost${port} `)
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -4,7 +4,7 @@ mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUn
 const db = mongoose.connection
 
 db.on('error', () => {
-  console.log('monodb error!')
+  console.log('mongodb error!')
 })
 db.once('open', () => {
   console.log('mongodb connected!')
