4f3ef76f5b9d137ec80c1635ecace3c8f361e55b
VanessaLin9 <vanessa7591@gmail.com>
2021-07-09 07:24:20 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -30,26 +30,46 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() //取出Todo model 裡的所有資料
     .lean() //把Mongoose 的 model 物件轉換成乾淨的 JS 資料陣列
-    .then( todos => res.render('index', {todos})) //將資料傳給 index 樣板, 這邊{todos}是{todos: todo}的縮寫
+    .then(todos => res.render('index', { todos })) //將資料傳給 index 樣板, 這邊{todos}是{todos: todo}的縮寫
     .catch(error => console.error(error)) //錯誤處理
 })
 
-app.get('/todos/new', (req,res) => {
+app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
 
-app.post('/todos', (req,res) => {
+app.post('/todos', (req, res) => {
   const name = req.body.name //從 req.body 拿出裡面的 name資料
-  return Todo.create({name}) //存入資料庫
+  return Todo.create({ name }) //存入資料庫
     .then(() => res.redirect('/')) //新增完成後導回首頁
     .catch(error => console.log(error))
 })
 
-app.get('/todos/:id', (req,res) => {
+app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
-    .then((todo) => res.render('detail', {todo}))
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
