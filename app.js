ad4128877beeb2b3362cc7f8fb4a0e4f2b871429
VanessaLin9 <vanessa7591@gmail.com>
2021-07-13 13:05:07 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const Todo = require('./models/todo') //載入todo model
 //載入method-override
 const methOverride = require('method-override')
 
+// 引用路由器
+const routes = require('./routes')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) //建立一個名為'hbs'的樣板引擎, 並且傳入exphbs 與相關參數
 app.set('view engine', 'hbs') //啟用樣板引擎 hbs
 
@@ -33,63 +36,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 //設定每一筆請求都會通過method override進行前處理
 app.use(methOverride('_method'))
 
-
-app.get('/', (req, res) => {
-  Todo.find() //取出Todo model 裡的所有資料
-    .lean() //把Mongoose 的 model 物件轉換成乾淨的 JS 資料陣列
-    .sort({ _id: 'asc' }) //根據ID做正序(ascending)排列, 反序是'desc'(desscending)
-    .then(todos => res.render('index', { todos })) //將資料傳給 index 樣板, 這邊{todos}是{todos: todo}的縮寫
-    .catch(error => console.error(error)) //錯誤處理
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name //從 req.body 拿出裡面的 name資料
-  return Todo.create({ name }) //存入資料庫
-    .then(() => res.redirect('/')) //新增完成後導回首頁
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// 將 request 導入路由器
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Express is listen on localhost${port} `)
---
+++ routes/modules/home.js
@@ -0,0 +1,19 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+// 引用 Todo model
+const Todo = require('../../models/todo')
+
+
+// 定義首頁路由
+router.get('/', (req, res) => {
+  Todo.find() //取出Todo model 裡的所有資料
+    .lean() //把Mongoose 的 model 物件轉換成乾淨的 JS 資料陣列
+    .sort({ _id: 'asc' }) //根據ID做正序(ascending)排列, 反序是'desc'(desscending)
+    .then(todos => res.render('index', { todos })) //將資料傳給 index 樣板, 這邊{todos}是{todos: todo}的縮寫
+    .catch(error => console.error(error)) //錯誤處理
+})
+
+
+// 匯出路由模組
+module.exports = router
